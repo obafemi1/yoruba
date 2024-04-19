c25c408b422a6a71bc8a84ae2085f32b297c89d1
obafemi <obare91@icloud.com>
2024-04-18 20:31:09 -0500

    new changes

--- Homework/src/routes/home/+page.svelte
+++ Homework/src/routes/home/+page.svelte
@@ -2,5 +2,19 @@
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
-<img src="th-image.jpg" alt="The Image" />
-<button class="btn">Gear Up</button>
+
+<img src="D:\Pong\IMG_3716.JPG" alt="The Image" />
+<button class="btn" onclick="window.location.href=`next-page.html`">Gear Up</button>
+
+<img src="file:"D:\Pong\IMG_3737.JPG" alt="Background Image 1" />
+<button class="btn" onclick="window.location.href='D:\Pong'">Ready</button>
+
+
+<img src="file:"D:\Pong\IMG_3739.JPG" alt="Background Image 2" />
+<button class="btn" onclick="window.location.href='D:\Pong'">Start</button>
+
+<img src="file:"D:\Pong\IMG_3742.JPG" alt="Background Image 3" />
+<button class="btn" onclick="window.location.href='D:\Pong'">Try Again</button>
+
+
+
