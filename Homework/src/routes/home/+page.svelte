720bf1e865b31d914eccdc439bc8082a7ac7fc16
obafemi <obare91@icloud.com>
2024-04-29 20:43:36 -0500

    4/49/24

--- Homework/src/routes/home/+page.svelte
+++ Homework/src/routes/home/+page.svelte
@@ -10,6 +10,7 @@
 	let h = 600;
 	const PADDLE_SPEED = 5; //change as needed
 	const SCORE_TO_WIN = 5;
+	
 
 	onMount(() => {
 		ball = document.querySelector('.ball');
@@ -79,29 +80,29 @@
 		}
 
 		function handleKeyDown(event) {
-			const keyPressed = event.key.toLowerCase();
-			if (keyPressed === `arrowup` || keyPressed === `w`) {
-				// Move left paddle up
-				leftPaddle.style.top = Math.max(parseInt(leftPaddle.style.top) - PADDLE_SPEED, 0) + `px`;
-			} else if (keyPressed === `arrowdown` || keyPressed === `s`) {
-				// Move left paddle down
-				leftPaddle.style.top =
-					Math.min(
-						parseInt(leftPaddle.style.top) + PADDLE_SPEED,
-						container.offsetHeight - leftPaddle.offsetHeight
-					) + `px`;
-			} else if (keyPressed === `arrowup`) {
-				// Move right paddle up
-				rightPaddle.style.top = Math.max(parseInt(rightPaddle.style.top) - PADDLE_SPEED, 0) + `px`;
-			} else if (keyPressed === `arrowdown`) {
-				// Move right paddle down
-				rightPaddle.style.top =
-					Math.min(
-						parseInt(rightPaddle.style.top) + PADDLE_SPEED,
-						container.offsetHeight - rightPaddle.offsetHeight
-					) + `px`;
-			}
-		}
+  const keyPressed = event.key.toLowerCase();
+  if (keyPressed === `arrowup` || keyPressed === `w`) {
+    // Move left paddle up
+    leftPaddle.style.top = Math.max(parseInt(leftPaddle.style.top) - PADDLE_SPEED, 0) + `px`;
+  } else if (keyPressed === `arrowdown` || keyPressed === `s`) {
+    // Move left paddle down
+    leftPaddle.style.top =
+      Math.min(
+        parseInt(leftPaddle.style.top) + PADDLE_SPEED,
+        container.offsetHeight - leftPaddle.offsetHeight
+      ) + `px`;
+  } else if (keyPressed === `arrowup` || keyPressed === `up`) {
+    // Move right paddle up
+    rightPaddle.style.top = Math.max(parseInt(rightPaddle.style.top) - PADDLE_SPEED, 0) + `px`;
+  } else if (keyPressed === `arrowdown` || keyPressed === `down`) {
+    // Move right paddle down
+    rightPaddle.style.top =
+      Math.min(
+        parseInt(rightPaddle.style.top) + PADDLE_SPEED,
+        container.offsetHeight - rightPaddle.offsetHeight
+      ) + `px`;
+  }
+}
 		function resetScores() {
 			leftScore = 0;
 			rightScore = 0;
